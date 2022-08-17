f99bbae4a97f1cc4aab507278f396420f8572800
s.torigoe <torigoe@lunacates.com>
2022-08-17 11:28:40 +0900

    test dev branch marge and pull

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+// 追加コード
+resource "aws_subnet" "test-subnet-a" {
+  vpc_id     = aws_vpc.test-vpc.id
+  cidr_block = "10.0.0.0/28"
+  availability_zone = "ap-northeast-1a"
 
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = "ap-northeast-1"
+  tags = {
+    Name = "test-subnet-a"
+  }
 }
 
-resource "aws_vpc" "test-vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_subnet" "test-subnet-c" {
+  vpc_id     = aws_vpc.test-vpc.id
+  cidr_block = "10.0.0.16/28"
+  availability_zone = "ap-northeast-1c"
+
   tags = {
-    Name = "test-vpc"
+    Name = "test-subnet-c"
   }
 }
